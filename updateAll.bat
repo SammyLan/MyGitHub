0690d35f2eca94dc90660ff2eb577508a99a4923
SammyLan <sammylan234@gmail.com>
2018-02-23 13:28:33 +0800

    updateAll.bat

--- updateAll.bat
+++ updateAll.bat
@@ -1,11 +1,15 @@
 @set CUR_DIR=%~dp0
 @cd %CUR_DIR%
 
-@rem git submodule foreach --recursive git submodule init 
-@rem git submodule foreach --recursive git submodule update
+@rem https://www.cnblogs.com/nicksheng/p/6201711.html
+@git submodule init
+@git submodule update
+@git submodule foreach git pull
 
 @for /f "delims=" %%i in ('dir /B /A:D "%CUR_DIR%"') do (
-	@cd %CUR_DIR%%%i
-	@git checkout master
+	@if not "%%i"==".git" (
+		@cd /D %CUR_DIR%%%i
+		@git checkout master
+	)
 )
 @pause
